ce242cfd11b72fad38612e7a98486d1bd13f10fd
eilamg <eilamg@post.bgu.ac.il>
2020-12-26 22:36:39 +0200

    First commit

---
+++ client.py
@@ -0,0 +1,22 @@
+import scapy
+import struct 
+
+
+def look_for_server():
+    pass
+
+
+def connect_to_server(ip):
+    pass
+
+
+def play_with_server(server_socket):
+    pass
+
+
+if __name__ == "__main__":
+    while True:
+        server_ip = look_for_server()
+        server_socket = connect_to_server(server_ip)
+        play_with_server(server_socket)
+
---
+++ server.py
@@ -0,0 +1,3 @@
+print("Hello World")
+x= "Eilam"
+print("Oded")
