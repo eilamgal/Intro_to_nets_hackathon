9d4549b0515fb991a231dd3d9c93c69fa4c7f91f
oded <odedodge@gmail.com>
2020-12-28 22:16:43 +0200

    server/client updates

--- client.py
+++ client.py
@@ -1,8 +1,25 @@
 import struct 
+import socket
 
 
 def look_for_server():
-    pass
+    
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    # Set broadcasting mode
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    client.bind(("", 13117))
+
+    while True:
+        try:
+            data, addr = client.recvfrom(10)
+            cookie, msg_type, port_number  = struct.unpack('IBH', data)
+            if cookie == 0xfeedbeef and msg_type == 0x2 and port_number > 0:
+                print("received ", hex(cookie), hex(msg_type), port_number,"from", addr[0])
+                return addr[0]
+            else:
+                print("Bad argument received!")
+        except (OSError, struct.error) : 
+            print("Unexpected message format")
 
 
 def connect_to_server(ip):
--- server.py
+++ server.py
@@ -1,13 +1,44 @@
 import struct
 import socket
+import time
+import scapy
+import queue
 
-# packed = struct.pack('IBH',0xfeedbeef,0x2,2018)
-# print(packed)
-# cookie, msg_type, port_number  = struct.unpack('IBH', packed)
-# print(hex(cookie), msg_type, port_number)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-print(socket.gethostname())
+def broadcast(time_limit=10, interval=1, tcp_port=40440):
+    start_time = time.time()
+
+    socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    while True:
+        try:
+            socket_server.bind((socket.gethostname(), tcp_port))
+            break
+        except Exception as massage: 
+            print('Bind failed. Message', massage) 
+
+    udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    # Set broadcasting mode
+    udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # Set a timeout 
+    udp_server.settimeout(0.3)
+
+    # i = 0
+    while time.time() - start_time < time_limit:
+        packed = struct.pack('IBH', 0xfeedbeef, 0x2, tcp_port)  # ? +i ? Message to broadcast : Magic coockie, message type, and port to connect through TCP
+        # i += 1  # next client to 
+        try:
+            udp_server.sendto(packed, (socket.gethostname(), 13117))
+        except socket.timeout:
+            print("Broadcast timout!")
+            
+        time.sleep(interval)
+
+
+
+if __name__ == "__main__":
+    thread_1 = broadcast()
+    thread_2 = listen_for_clients()
+    play()
 
 
