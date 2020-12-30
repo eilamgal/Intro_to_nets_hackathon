6e9b51bf45c46a39c7ff00f83b37c71a6556dd8c
oded <odedodge@gmail.com>
2020-12-30 21:46:30 +0200

    fix

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ def look_for_server():
         try:
             data, addr = client.recvfrom(10)
             cookie, msg_type, port_number  = struct.unpack('IBH', data)
-            if cookie == 0xfeedbeef and msg_type == 0x2 and port_number > 0:  #  == 2018
+            if cookie == 0xfeedbeef and msg_type == 0x2 and port_number == 2018:  #  == 2018
                 print("received ", hex(cookie), hex(msg_type), port_number,"from", addr[0])
                 return addr[0], port_number
             # else:
